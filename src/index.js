4936f0ee1469397f99dce7fd72a3f2d72dfae6b8
Mohamed Jadib <jadibdev@gmail.com>
2022-06-14 22:28:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import renderHeadline from './headline.js';
+// import renderHeadline from './headline.js';
 import renderMainSectionComponent from './mainSection.js';
 
 // renderHeadline();
